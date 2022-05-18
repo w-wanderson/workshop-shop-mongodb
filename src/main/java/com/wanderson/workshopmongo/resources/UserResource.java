536bee04b9edbbe21c3b4d58423a825958069338
w-wanderson <wandersondasilvasantos18@hotmail.com>
2022-05-18 15:12:50 -0300

    Inserção de usuário com POST

--- src/main/java/com/wanderson/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wanderson/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.wanderson.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wanderson.workshopmongo.domain.User;
 import com.wanderson.workshopmongo.dto.UserDto;
@@ -30,7 +33,15 @@ public class UserResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDto> findByid(@PathVariable String id) {
-		User obj = service.findByid(id);				
+		User obj = service.findByid(id);
 		return ResponseEntity.ok().body(new UserDto(obj));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void>  insert(@RequestBody UserDto objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/wanderson/workshopmongo/services/UserService.java
+++ src/main/java/com/wanderson/workshopmongo/services/UserService.java
@@ -7,22 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wanderson.workshopmongo.domain.User;
+import com.wanderson.workshopmongo.dto.UserDto;
 import com.wanderson.workshopmongo.repository.UserRepository;
 import com.wanderson.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findByid(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
